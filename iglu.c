7402eacd1c79541e52e5a3db807a56c3d3a41b2a
Rodrigo Arboleda <rodrigo.arboleda@usp.br>
2021-05-27 16:26:07 -0300

    Comment added

--- iglu.c
+++ iglu.c
@@ -1,5 +1,16 @@
 #include "iglu.h"
 
+/*
+Gera os pontos referente ao objeto e adiciona os valores no vetor passado.
+Ao adicionar os pontos no vetor vertices, ele utiliza o parametro vertice
+inicial para saber o local onde esses pontos devem ser salvos. No fim retorna
+uma variavel do tipo OBJ com informacoes referentes a este objeto criado
+@PARAMETROS
+    coordenadas *vertices - endereco do vetor para armazenar os vertices gerados
+    int vertice_inicial - posisao no vetor vertices para o primeiro vertice gerado
+@RETORNO
+    OBJ - retorna o tipo OBJ com informacoes sobre o objeto criado
+*/
 OBJ cria_iglu(coordenadas*vertices , int vertice_inicial)
 {
     const int num_vert_circ = 32;
@@ -84,6 +95,21 @@ OBJ cria_iglu(coordenadas*vertices , int vertice_inicial)
 
 }
 
+/*
+Esta funcao envia a matriz referente ao objeto para a GPU alem de definir a cor
+a ser utilizada. Apois isso invoca os metodos para desenhar este objeto. Esta funcao
+pode desenhar um iglu referenciado por OBJ iglu. Vale dizer que pode ser desenhado
+um objeto tanto como colorido, cinza e preto.
+@PARAMETROS
+    OBJ iglu - variavel que guarda informacoes referente ao objeto, gerada quando o objeto eh criado
+    GLint loc_color - variavel linkada a variavel de cor na GPU
+    GLint loc_matriz - variavel linkada a variavel de matriz na GPU
+    int cor - define se a deve ser colorido, cinza ou preto
+              0 - colorido
+              1 - cinza
+              2 - preto
+              Esta variavel esta linkada com base.h, utilizando um enum com os nomes das cores
+*/
 void desenha_iglu(OBJ iglu, GLint loc_color, GLint loc_matriz, int cor)
 {
     //chamar a funcao para montar e enviar a matriz de transformacao para gpu
